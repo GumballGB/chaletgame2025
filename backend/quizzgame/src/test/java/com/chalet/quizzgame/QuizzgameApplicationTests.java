7d20e4dea0fa07a4ab074a16231005612591acf3
Anslean <albertanslean@gmail.com>
2025-05-13 00:06:35 -0400

    Backend Spring Boot stuff
    The beginning

---
+++ backend/quizzgame/src/test/java/com/chalet/quizzgame/QuizzgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalet.quizzgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
